adbeac134b8590a2cbfe1331abb8790d083e8f6e
Vijayaraghavan Murali <vm4422@gmail.com>
2017-02-15 11:02:39 -0600

    Change in variational objective (cost) function
    weight_loss now divides latent_loss rather than multiplying
    generation_loss

--- variational/model.py
+++ variational/model.py
@@ -42,7 +42,7 @@ class Model():
         self.generation_loss = tf.nn.seq2seq.sequence_loss([logits],
                                     [tf.reshape(self.targets, [-1])],
                                     [tf.ones([args.batch_size * args.max_ast_depth])])
-        self.cost = tf.reduce_mean(args.weight_loss * self.generation_loss + self.latent_loss)
+        self.cost = tf.reduce_mean(self.generation_loss + self.latent_loss/args.weight_loss)
         self.train_op = tf.train.AdamOptimizer(args.learning_rate).minimize(self.cost)
 
         var_params = [np.prod([dim.value for dim in var.get_shape()])
