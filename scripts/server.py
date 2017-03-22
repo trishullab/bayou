6b05a1c8dfa1e701ff967eceff437219fb127bcc
Vijayaraghavan Murali <vm4422@gmail.com>
2017-03-22 14:45:26 -0500

    Server script now logs

--- scripts/server.py
+++ scripts/server.py
@@ -5,23 +5,27 @@ import argparse
 import os
 import json
 import threading
+import time
 
 from variational.predict_ast import VariationalPredictor
 from variational.data_reader import sub_sequences
 from variational.utils import get_keywords
 
 def start_server(args):
-    with tf.Session() as sess:
-        assert not os.path.exists(args.pipe), \
-            'I think server is already running! If not, delete pipe: {}'.format(args.pipe)
-        os.mkfifo(args.pipe)
+    assert not os.path.exists(args.pipe), \
+        'I think server is already running! If not, delete pipe: {}'.format(args.pipe)
+    os.mkfifo(args.pipe)
+    with tf.Session() as sess, open(args.log, 'w') as logfile:
         predictor = VariationalPredictor(args.save_dir, sess)
-        print('Server started and listening to: {}'.format(args.pipe))
+        log('Server started and listening to: {}'.format(args.pipe), logfile)
+        req = 0
         try:
             while True:
                 with open(args.pipe) as pipe:
                     content = pipe.read()
                     threading.Thread(target=serve, args=(content, predictor)).start()
+                    req += 1
+                    log('Served request {}\n{}'.format(req, content), logfile)
         finally:
             os.remove(args.pipe)
 
@@ -31,7 +35,7 @@ def serve(content, predictor):
     keywords = contents[1].split()
     sequences = []
     if len(contents) > 2:
-        sequences = sub_sequences(json.loads('\n'.join(contents[2:])))
+        sequences = sub_sequences(json.loads('\n'.join(contents[2:])), predictor.model.args)
     keywords = list(set(keywords + get_keywords(sequences)))
     keywords = [k for k in keywords if k in predictor.input_vocab_kws]
 
@@ -47,11 +51,19 @@ def serve(content, predictor):
     with open(outpipe, 'w') as f:
         json.dump({ 'asts': asts }, f, indent=2)
 
+def log(p, logfile):
+    t = time.asctime()
+    logfile.write('{} :: {}\n'.format(t, p))
+    logfile.flush()
+    os.fsync(logfile)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_dir', type=str, required=True,
                        help='model directory to laod from')
     parser.add_argument('--pipe', type=str, required=True,
                        help='pipe file to listen to')
+    parser.add_argument('--log', type=str, default='log.out',
+                       help='log file')
     args = parser.parse_args()
     start_server(args)
