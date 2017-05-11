6e9c52dd99c3bdfe86dbc1bbba68b7b8817ab70e
Vijayaraghavan Murali <vm4422@gmail.com>
2017-05-11 19:20:44 -0400

    Added LDA training for keywords and types

---
+++ src/ml/bayou/lda/model.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pickle
+from collections import OrderedDict
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+
+class LDA():
+
+    def __init__(self, args=None, from_file=None):
+        """ Initialize LDA model from either arguments or a file. If both are
+            provided, file will be used."""
+        assert args or from_file, 'Improper initialization of LDA model'
+        if from_file is not None:
+            self.model, self.vectorizer = pickle.load(from_file)
+        else:
+            self.vectorizer = TfidfVectorizer(lowercase=False, token_pattern=u'[^;]+')
+            self.model = LatentDirichletAllocation(args.ntopics, doc_topic_prior=args.alpha,
+                                                   learning_method='batch', max_iter=100, verbose=1,
+                                                   evaluate_every=1, max_doc_update_iter=100,
+                                                   mean_change_tol=1e-5)
+
+    def top_words(self, ntop_words):
+        features = self.vectorizer.get_feature_names()
+        words = [OrderedDict([(features[i], topic[i]) for i in topic.argsort()[:-ntop_words - 1:-1]])
+                     for topic in self.model.components_]
+        return words
+
+    def train(self, data):
+        vect = self.vectorizer.fit_transform(data)
+        self.model.fit(vect)
+        # note: normalizing does not change subsequent inference, provided no further training is done
+        self.model.components_ /= self.model.components_.sum(axis=1)[:, np.newaxis]
+
+    def infer(self, data):
+        vect = self.vectorizer.transform(data)
+        dist = self.model.transform(vect)
+        assert vect.shape[0] == dist.shape[0]
+
+        samples = [list(doc_topic_dist) for doc_topic_dist in dist]
+        return samples
+
---
+++ src/ml/bayou/lda/train.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import json
+import pickle
+import argparse
+
+from bayou.lda.model import LDA
+
+def train(clargs):
+    print('Reading data file...')
+    data = get_data(clargs.input_file[0], clargs.evidence)
+
+    ok = 'r'
+    while ok == 'r':
+        model = LDA(args=clargs)
+        model.train(data)
+        top_words = model.top_words(clargs.top)
+        for i, words in enumerate(top_words):
+            print('\nTop words in Topic#{:d}'.format(i))
+            for w in words:
+                print('{:.2f} {:s}'.format(words[w], w))
+        print('\nOK with the model (y(es)/n(o)/r(edo))? ', end='')
+        ok = sys.stdin.readline().rstrip('\n')
+
+    if ok == 'y':
+        print('Saving model to {:s}'.format(os.path.join(clargs.save, 'lda.pkl')))
+        with open(os.path.join(clargs.save, 'lda.pkl'), 'wb') as fmodel:
+            pickle.dump((model.model, model.vectorizer), fmodel)
+
+
+def get_data(input_file, evidence):
+    with open(input_file) as f:
+        js = json.loads(f.read())
+    data = []
+    nprograms = len(js['programs'])
+
+    for i, program in enumerate(js['programs']):
+        bow = set(program[evidence])
+        data.append(';'.join(bow))
+        print('Gathering data for LDA: {:5d}/{:d} programs'.format(i+1, nprograms), end='\r')
+    print()
+    return data
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argparser.add_argument('input_file', type=str, nargs=1,
+                           help='input JSON file')
+    argparser.add_argument('--ntopics', type=int, required=True,
+                           help='run LDA with n topics')
+    argparser.add_argument('--evidence', choices=['keywords', 'types'], required=True,
+                           help='the type of evidence for which LDA is run')
+    argparser.add_argument('--save', type=str, default='save',
+                           help='directory to store LDA model')
+    argparser.add_argument('--alpha', type=float, default=0.1,
+                           help='initial alpha value')
+    argparser.add_argument('--top', type=int, default=5,
+                           help='top-k words to print from each topic')
+    clargs = argparser.parse_args()
+    train(clargs)
