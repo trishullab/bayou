5c99466dce0a2512aba70dc032d921294921d46a
Vijayaraghavan Murali <vm4422@gmail.com>
2017-05-24 21:15:46 -0500

    LDA will return zero vector if document is empty
    To comply with the recent change in encoder about handling empty evidences.

--- src/ml/bayou/lda/model.py
+++ src/ml/bayou/lda/model.py
@@ -18,29 +18,31 @@ class LDA():
         else:
             self.vectorizer = TfidfVectorizer(lowercase=False, token_pattern=u'[^;]+')
             self.model = LatentDirichletAllocation(args.ntopics, doc_topic_prior=args.alpha,
-                                                   learning_method='batch', max_iter=100, verbose=1,
-                                                   evaluate_every=1, max_doc_update_iter=100,
-                                                   mean_change_tol=1e-5)
+                                                   learning_method='batch', max_iter=100,
+                                                   verbose=1, evaluate_every=1,
+                                                   max_doc_update_iter=100, mean_change_tol=1e-5)
 
-    def top_words(self, ntop_words):
+    def top_words(self, n):
         features = self.vectorizer.get_feature_names()
-        words = [OrderedDict([(features[i], topic[i]) for i in topic.argsort()[:-ntop_words - 1:-1]])
+        words = [OrderedDict([(features[i], topic[i]) for i in topic.argsort()[:-n - 1:-1]])
                  for topic in self.model.components_]
         return words
 
-    def train(self, data):
-        data = [';'.join(bow) for bow in data]
+    def train(self, docs):
+        data = [';'.join(bow) for bow in docs]
         vect = self.vectorizer.fit_transform(data)
         self.model.fit(vect)
-        # note: normalizing does not change subsequent inference, provided no further training is done
+        # normalizing does not change subsequent inference, provided no further training is done
         self.model.components_ /= self.model.components_.sum(axis=1)[:, np.newaxis]
 
-    def infer(self, data):
-        data = [';'.join(bow) for bow in data]
+    def infer(self, docs):
+        data = [';'.join(bow) for bow in docs]
         vect = self.vectorizer.transform(data)
         dist = self.model.transform(vect)
         assert vect.shape[0] == dist.shape[0]
 
-        samples = [list(doc_topic_dist) for doc_topic_dist in dist]
+        # NOTE: if a document is empty, this method returns a zero topic-dist vector
+        samples = [list(doc_topic_dist) if not bow == [] else ([0.] * self.model.n_topics)
+                   for bow, doc_topic_dist in zip(docs, dist)]
         return samples
 
