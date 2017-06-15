46e50ac452564e5313686246549840663db9b981
Vijayaraghavan Murali <vm4422@gmail.com>
2017-06-15 18:27:57 -0500

    Updated sythesizer tests

--- test/pl/synthesizer/TestIO1.java
+++ test/pl/synthesizer/TestIO1.java
@@ -2,7 +2,7 @@ import edu.rice.bayou.annotations.Evidence;
 
 public class TestIO1 {
 
-    @Evidence(apicalls = {"readLine", "close"})
+    @Evidence(apicalls = {"readLine", "ready"})
     void __bayou_fill(String file) {
 
     }   
--- test/pl/synthesizer/TestIO_exception.java
+++ test/pl/synthesizer/TestIO_exception.java
@@ -2,7 +2,7 @@ import edu.rice.bayou.annotations.Evidence;
 
 public class TestIO_exception {
 
-    @Evidence(apicalls = {"readLine", "printStackTrace"})
+    @Evidence(apicalls = {"readLine", "printStackTrace", "close"})
     @Evidence(context = {"String"})
     void __bayou_fill(String file) {
 
