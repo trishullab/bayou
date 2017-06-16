f8861fb6e87d5459cba16d4a13f981abfe757b8e
Vijayaraghavan Murali <vm4422@gmail.com>
2017-06-16 16:45:15 -0500

    Added new Android examples for synthesizer

---
+++ test/pl/synthesizer/TestCamera.java
@@ -0,0 +1,11 @@
+import edu.rice.bayou.annotations.Evidence;
+
+public class TestCamera {
+
+    @Evidence(apicalls = {"startPreview"})
+    @Evidence(types = {"Camera"})
+    void __bayou_fill(int w, int h) {
+
+    }   
+
+}
---
+++ test/pl/synthesizer/TestSpeech.java
@@ -0,0 +1,12 @@
+import edu.rice.bayou.annotations.Evidence;
+import android.content.Context;
+
+public class TestCamera {
+
+    @Evidence(types = {"SpeechRecognizer"})
+    @Evidence(context = {"Context"})
+    void __bayou_fill(Context context) {
+
+    }   
+
+}
