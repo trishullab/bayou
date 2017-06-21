068765a1cc1a2d02f2465b05b9f692f0916567cb
Vijayaraghavan Murali <vm4422@gmail.com>
2017-06-20 19:04:32 -0500

    Moved split.py and merge.py to scripts

--- scripts/merge.py
+++ scripts/merge.py
@@ -8,6 +8,7 @@ import os
 import json
 import argparse
 
+
 def merge(args):
     programs = []
     for filename in os.listdir(args.folder[0]):
@@ -15,13 +16,13 @@ def merge(args):
             js = json.load(f)
         programs += (js['programs'])
     with open(args.output_file, 'w') as f:
-        json.dump({ 'programs': programs }, f, indent=2)
+        json.dump({'programs': programs}, f, indent=2)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('folder', type=str, nargs=1,
-                       help='folder where all JSON files are stored')
+                        help='folder where all JSON files are stored')
     parser.add_argument('--output_file', type=str, required=True,
-                       help='file to output merged data')
+                        help='file to output merged data')
     args = parser.parse_args()
     merge(args)
