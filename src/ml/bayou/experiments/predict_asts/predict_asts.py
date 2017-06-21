898d0f8841d68f1eef08ac8787e01393054beb60
Vijayaraghavan Murali <vm4422@gmail.com>
2017-06-20 19:03:30 -0500

    Updated predict_ast experiment

--- src/ml/bayou/experiments/predict_asts/predict_asts.py
+++ src/ml/bayou/experiments/predict_asts/predict_asts.py
@@ -2,88 +2,70 @@ import tensorflow as tf
 import argparse
 import json
 import time
-import math
-import random
 
-from experimental.encdec.predict_ast import Predictor
-from variational.predict_ast import VariationalPredictor
-from variational.data_reader import get_seqs, sub_sequences
+import bayou.core.infer
+import bayou.experiments.nonbayesian.infer
+import bayou.experiments.low_level_evidences.infer
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_file', type=str, nargs=1,
-                       help='input data file')
-    parser.add_argument('--save_dir', type=str, required=True,
-                       help='directory where model is saved')
-    parser.add_argument('--model', type=str, required=True, choices=['encdec', 'variational'],
-                       help='the type of model')
-    parser.add_argument('--n', type=int, default=1,
-                       help='number of ASTs to generate for each set of sequences')
-    parser.add_argument('--infer_seqs', type=float, default=1.,
-                       help='percentage of sequences (range (0,1]) to provide for inference')
-    parser.add_argument('--seed', type=int, default=1234,
-                       help='seed to use for random')
-    parser.add_argument('--output_file', type=str, default=None,
-                       help='output file to print predicted ASTs')
-    args = parser.parse_args()
-    if args.infer_seqs <= 0. or args.infer_seqs > 1.:
-        parser.error('infer_seqs must be in range (0,1]')
-    random.seed(args.seed)
-    print(args)
+TIMEOUT = 20  # seconds per query
+
+
+def main(clargs):
+    with open(clargs.input_file[0]) as f:
+        js = json.load(f)
+    programs = js['programs']
 
     with tf.Session() as sess:
-        if args.model == 'encdec':
-            predictor = Predictor(args.save_dir, sess)
+        if clargs.model == 'bayesian':
+            p_type = bayou.core.infer.BayesianPredictor
+        elif clargs.model == 'nonbayesian':
+            p_type = bayou.experiments.nonbayesian.infer.NonBayesianPredictor
+        elif clargs.model == 'low_level_evidences':
+            p_type = bayou.experiments.low_level_evidences.infer.BayesianPredictor
         else:
-            predictor = VariationalPredictor(args.save_dir, sess)
+            raise TypeError('invalid type of model specified')
+        print('Loading model...')
+        predictor = p_type(clargs.save, sess)
 
-        data = gather_data(args.input_file[0], predictor.model.args, args.infer_seqs)
-        programs = []
-        for j, (filename, given_seqs, unseen_seqs, org_ast) in enumerate(data):
-            try:
-                predictor_input = given_seqs if args.model == 'encdec' \
-                                             else predictor.psi_from_seqs(given_seqs)
-            except TypeError:
-                continue
-            asts = []
-            for i in range(args.n):
+        for i, program in enumerate(programs):
+            start = time.time()
+            asts, counts = [], []
+            for j in range(100):
+                if time.time() - start > TIMEOUT:
+                    break
                 try:
-                    ast = predictor.generate_ast(predictor_input)[0]
-                    asts.append(ast)
-                except (AssertionError,  TypeError):
+                    ast = predictor.infer(program)
+                except AssertionError:
                     continue
-            js_dict = { 'file': filename,
-                        'original_ast': org_ast,
-                        'given_sequences': given_seqs,
-                        'unseen_sequences': unseen_seqs,
-                        'predicted_asts': asts } 
-            programs.append(js_dict)
-            print('Done with {} programs'.format(j))
+                try:
+                    counts[asts.index(ast)] += 1
+                except ValueError:
+                    asts.append(ast)
+                    counts.append(1)
+            for ast, count in zip(asts, counts):
+                ast['count'] = count
+            asts.sort(key=lambda x: x['count'], reverse=True)
+            program['out_asts'] = asts[:10]
+            print('Program {}, {} ASTs, {:.2f}s'.format(
+                i, len(program['out_asts']), time.time() - start))
 
-        if args.output_file is None:
-            print(json.dumps({ 'programs': programs }, indent=2))
+        if clargs.output_file is None:
+            print(json.dumps({'programs': programs}, indent=2))
         else:
-            with open(args.output_file, 'w') as f:
-                json.dump({ 'programs': programs }, f, indent=2)
-
-def gather_data(input_file, saved_args, infer_seqs):
-    filenames, given_seqs, unseen_seqs, asts = [], [], [], []
-    with open(input_file) as f:
-        js = json.load(f)
-    for program in js['programs']:
-        if 'ast' not in program or 'sequences' not in program:
-            continue
-        seqs_program = get_seqs(program['sequences'])
-        try:
-            sub_seqs = sub_sequences(seqs_program, saved_args)
-        except AssertionError:
-            continue
-        selected_seqs = sub_seqs[-math.ceil(len(sub_seqs)*infer_seqs):]
-        given_seqs.append(selected_seqs)
-        unseen_seqs.append([seq for seq in sub_seqs if seq not in given_seqs[-1]])
-        asts.append(program['ast'])
-        filenames.append(program['file'])
-    return zip(filenames, given_seqs, unseen_seqs, asts)
+            with open(clargs.output_file, 'w') as f:
+                json.dump({'programs': programs}, f, indent=2)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('input_file', type=str, nargs=1,
+                        help='input data file')
+    parser.add_argument('--save', type=str, required=True,
+                        help='directory to load model from')
+    parser.add_argument('--model', type=str, required=True,
+                        choices=['bayesian', 'nonbayesian', 'low_level_evidences'],
+                        help='the type of model')
+    parser.add_argument('--output_file', type=str, default=None,
+                        help='output file to print predicted ASTs')
+    clargs = parser.parse_args()
+    print(clargs)
+    main(clargs)
