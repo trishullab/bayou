f123f5b0ee7f0d3355f248365ee3eca2dd41d5c4
Vijayaraghavan Murali <vm4422@gmail.com>
2017-06-29 10:50:53 -0500

    Changed Evidence from interface to class

--- src/main/java/edu/rice/cs/caper/bayou/annotations/Evidence.java
+++ src/main/java/edu/rice/cs/caper/bayou/annotations/Evidence.java
@@ -1,6 +1,6 @@
 package edu.rice.cs.caper.bayou.annotations;
 
-public interface Evidence {
+public class Evidence {
     public static void apicalls(String... s) { }
     public static void types(String... s) { }
     public static void context(String... s) { }
