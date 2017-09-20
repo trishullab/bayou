c39bc22c688fd7c98a1271145c4250eaf38b5188
rmbarnett-rice <barnett@rice.edu>
2017-09-19 19:47:46 -0500

    change build.sh to use dependent build.sh

--- tool_files/build_binary_release/build.sh
+++ tool_files/build_binary_release/build.sh
@@ -16,16 +16,21 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+$SCRIPT_DIR/../build_scripts/build.sh
+
 cd ../maven_3_3_9/bayou
-VER="$(printf 'VERSION=${project.version}\n0\n' | mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate | grep '^VERSION' | cut -c9-)" # get the project version number... e.g. 1.1.0
-mvn clean package
-cp target/bayou-$VER-jar-with-dependencies.jar $SCRIPT_DIR
-cp -r ../../../src/main/python $SCRIPT_DIR
-cp -r ../../../src/main/resources $SCRIPT_DIR
-cp ../../../src/main/bash/binary_release/*.sh $SCRIPT_DIR
-cp -r target/bayou-$VER-jar-with-dependencies.jar $SCRIPT_DIR
-cp -r ../../../example_inputs $SCRIPT_DIR
-cd $SCRIPT_DIR
-mv bayou-$VER-jar-with-dependencies.jar bayou-$VER.jar
-zip -r bayou-$VER.zip bayou-1.0.0.jar example_inputs install_dependencies_apt.sh install_dependencies_mac.sh start_bayou.sh synthesize.sh python resources
-rm -r bayou-$VER.jar example_inputs install_dependencies_apt.sh install_dependencies_mac.sh start_bayou.sh synthesize.sh python resources
+VER="$(printf 'VERSION=${project.version}\n0\n' | mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate | grep '^VERSION' | cut -c9-)" # get the project version number... e.g 1.1.0 mvn clean package
+
+#mvn clean package
+#cp target/bayou-$VER-jar-with-dependencies.jar $SCRIPT_DIR
+#cp -r ../../../src/main/python $SCRIPT_DIR
+#cp -r ../../../src/main/resources $SCRIPT_DIR
+#cp ../../../src/main/bash/binary_release/*.sh $SCRIPT_DIR
+#cp -r target/bayou-$VER-jar-with-dependencies.jar $SCRIPT_DIR
+#cp -r ../../../example_inputs $SCRIPT_DIR
+#cd $SCRIPT_DIR
+#mv bayou-$VER-jar-with-dependencies.jar bayou-$VER.jar
+#echo $VER
+cd $SCRIPT_DIR/../build_scripts/out/
+zip -r $SCRIPT_DIR/bayou-$VER.zip *
+#rm -r bayou-$VER.jar example_inputs install_dependencies_apt.sh install_dependencies_mac.sh start_bayou.sh synthesize.sh python resources
