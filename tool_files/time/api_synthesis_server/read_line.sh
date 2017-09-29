1dcf9ba5f8ba1e259e50fb5529f5772f01a37b89
rmbarnett-rice <barnett@rice.edu>
2017-09-29 14:04:48 -0500

    update message format

--- tool_files/time/api_synthesis_server/read_line.sh
+++ tool_files/time/api_synthesis_server/read_line.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-time curl -H "Origin: http://askbayou.com" -d '{ "code" : "import edu.rice.cs.caper.bayou.annotations.Evidence; public class Test { void read(String file) {  Evidence.apicalls(\"readLine\"); } }" }'  http://127.0.0.1:8080/apisynthesis
+time curl -H "Origin: http://askbayou.com" -d '{ "max program count" : "2",  "code" : "public class Test { void read(String file) {  /// call: readLine\n } }" }'  http://127.0.0.1:8080/apisynthesis
--- tool_files/time/ast_server/dialog_evidence.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-time echo -e '\x00''\x00''\x00''\x59' { \"apicalls\": [ \"setTitle\", \"setMessage\" ], \"types\": [ \"AlertDialog\" ], \"context\": [] } | nc 127.0.0.1 8084
