acb31dc53deb69774990f3dc069c3d2b3945ee9d
Matt Barnett <barnett@rice.edu>
2017-10-10 19:37:26 -0500

    Squashed commit of the following
    commit c17c9fcb2813f093560955d165f6d14e93c4b685
    Merge: 411ff58 1dcf9ba
    Author: rmbarnett-rice <barnett@rice.edu>
    Date:   Tue Oct 10 19:08:56 2017 -0500
    
        restore file
    
    commit 411ff58e83eab66846912492e1f32326f8f56028
    Author: rmbarnett-rice <barnett@rice.edu>
    Date:   Tue Oct 10 19:08:03 2017 -0500
    
        update to include keywords type
    
    commit 8fd54f59f289ca066f908b7216aa29fa2efa3e4a
    Author: rmbarnett-rice <barnett@rice.edu>
    Date:   Fri Sep 29 14:04:48 2017 -0500
    
        update message format
    
    commit 1dcf9ba5f8ba1e259e50fb5529f5772f01a37b89
    Author: rmbarnett-rice <barnett@rice.edu>
    Date:   Fri Sep 29 14:04:48 2017 -0500
    
        update message format

--- tool_files/time/ast_server/dialog_evidence.sh
+++ tool_files/time/ast_server/dialog_evidence.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-time curl -H "Content-Type: application/json"  -d '{ "request type" : "generate asts", "max ast count" : 1  , "evidence": "{\"apicalls\": [ \"setTitle\", \"setMessage\" ], \"types\": [ \"AlertDialog\" ], \"context\": []}" }' http://localhost:8084/
+time curl -H "Content-Type: application/json"  -d '{ "request type" : "generate asts", "max ast count" : 1  , "evidence": "{\"apicalls\": [ \"setTitle\", \"setMessage\" ], \"types\": [ \"AlertDialog\" ], \"context\": [], \"keywords\": []}" }' http://localhost:8084/
