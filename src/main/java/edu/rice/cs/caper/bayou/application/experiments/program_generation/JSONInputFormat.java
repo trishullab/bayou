ca243105fede0d787acf659397adcd2f243b872d
Vijayaraghavan Murali <vm4422@gmail.com>
2017-10-14 00:43:23 -0500

    Implement sketch to AML compiler under default env

---
+++ src/main/java/edu/rice/cs/caper/bayou/application/experiments/program_generation/JSONInputFormat.java
@@ -0,0 +1,53 @@
+/*
+Copyright 2017 Rice University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package edu.rice.cs.caper.bayou.application.experiments.program_generation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.rice.cs.caper.bayou.core.dsl.*;
+import edu.rice.cs.caper.bayou.core.synthesizer.RuntimeTypeAdapterFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class JSONInputFormat {
+
+    public static List<DataPoint> readData(String file) throws IOException {
+        RuntimeTypeAdapterFactory<DASTNode> nodeAdapter = RuntimeTypeAdapterFactory.of(DASTNode.class, "node")
+                .registerSubtype(DAPICall.class)
+                .registerSubtype(DBranch.class)
+                .registerSubtype(DExcept.class)
+                .registerSubtype(DLoop.class)
+                .registerSubtype(DSubTree.class);
+        Gson gson = new GsonBuilder().serializeNulls()
+                .registerTypeAdapterFactory(nodeAdapter)
+                .create();
+        String s = new String(Files.readAllBytes(Paths.get(file)));
+        Data js = gson.fromJson(s, Data.class);
+
+        return js.programs;
+    }
+
+    static class Data {
+        List<DataPoint> programs;
+    }
+
+    static class DataPoint {
+        DSubTree ast;
+    }
+}
---
+++ src/main/java/edu/rice/cs/caper/bayou/application/experiments/program_generation/SketchToAMLCompiler.java
@@ -0,0 +1,77 @@
+/*
+Copyright 2017 Rice University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package edu.rice.cs.caper.bayou.application.experiments.program_generation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.rice.cs.caper.bayou.core.synthesizer.ParseException;
+import edu.rice.cs.caper.bayou.core.synthesizer.Parser;
+import edu.rice.cs.caper.bayou.core.synthesizer.Synthesizer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SketchToAMLCompiler {
+
+    String source =
+                    "import edu.rice.cs.caper.bayou.annotations.Evidence;\n" +
+                    "public class Test {\n" +
+                    "    void toSynthesize(String dummy) {\n" +
+                    "        {\n" +
+                    "            Evidence.apicalls(\"this is just a marker\");\n" +
+                    "        }\n" +
+                    "    }\n" +
+                    "}\n";
+
+    List<String> compile(String sketches) throws ParseException {
+        Parser parser = new Parser(source, System.getProperty("java.class.path"));
+        parser.parse();
+
+        Synthesizer synthesizer = new Synthesizer();
+        return synthesizer.execute(parser, sketches);
+    }
+
+    void compileData(String datafile, String outfile) throws IOException, ParseException {
+        List<JSONInputFormat.DataPoint> programs = JSONInputFormat.readData(datafile);
+        List<String> amlPrograms = new ArrayList<>();
+        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        int i = 0;
+        for (JSONInputFormat.DataPoint program : programs) {
+            List<String> output = compile("{ \"asts\": [" + gson.toJson(program.ast) + "]}");
+            if (output.size() != 1)
+                amlPrograms.add("ERROR");
+            else
+                amlPrograms.add(output.get(0));
+            i++;
+            System.out.println(String.format("done %d/%d", i, programs.size()));
+        }
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(outfile));
+        bw.write("{ \"programs\": [\n" + gson.toJson(amlPrograms) + "\n]}");
+        bw.close();
+    }
+
+    public static void main(String args[]) throws IOException, ParseException {
+        if (args.length != 2) {
+            System.err.println("Usage: sketchToAML DATA.json DATA-output.json");
+            System.exit(1);
+        }
+        new SketchToAMLCompiler().compileData(args[0], args[1]);
+    }
+}
