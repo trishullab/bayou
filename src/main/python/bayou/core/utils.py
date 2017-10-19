6a0e1259f55225de5e18f2782f1988294659e6dc
Vijayaraghavan Murali <vm4422@gmail.com>
2017-10-19 10:42:40 -0500

    Make encoder neural network multi-layered

--- src/main/python/bayou/core/utils.py
+++ src/main/python/bayou/core/utils.py
@@ -19,7 +19,7 @@ import tensorflow as tf
 
 CONFIG_GENERAL = ['latent_size', 'batch_size', 'num_epochs',
                   'learning_rate', 'print_step', 'alpha', 'beta']
-CONFIG_ENCODER = ['name', 'units', 'tile']
+CONFIG_ENCODER = ['name', 'units', 'num_layers', 'tile']
 CONFIG_DECODER = ['units', 'max_ast_depth']
 CONFIG_DECODER_INFER = ['chars', 'vocab', 'vocab_size']
 
