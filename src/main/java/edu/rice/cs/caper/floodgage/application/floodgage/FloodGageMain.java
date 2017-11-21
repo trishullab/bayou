ec0fb7f08272ffbb2559d2bc3b9920606e1b5e6b
R. Matt Barnett <barnett@rice.edu>
2017-11-21 15:27:11 -0600

    increased documentation

--- src/main/java/edu/rice/cs/caper/floodgage/application/floodgage/FloodGageMain.java
+++ src/main/java/edu/rice/cs/caper/floodgage/application/floodgage/FloodGageMain.java
@@ -1,30 +1,34 @@
+/*
+Copyright 2017 Rice University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package edu.rice.cs.caper.floodgage.application.floodgage;
 import edu.rice.cs.caper.floodgage.application.floodgage.synthesizer.SynthesizerBayou_1_1_0;
 import edu.rice.cs.caper.floodgage.application.floodgage.view.ViewConsole;
-import net.openhft.compiler.CompilerUtils;
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.simple.SimpleLogger;
 import org.apache.logging.log4j.status.StatusLogger;
-import org.junit.runner.Computer;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-
-
+/**
+ * The entry point class for the Floodgage tool.
+ */
 public class FloodGageMain
 {
-
     public static void main(String[] args) throws Exception
     {
+        // some of the libs we depend on hard coded Log4J into the source. So disable their logging
+        // to prevent warnings from being printed to the console
         StatusLogger.getLogger().setLevel(Level.OFF);
 
         if(args.length != 1)
@@ -36,9 +40,13 @@ public class FloodGageMain
         String bayouHostname = args[0];
         SynthesizerBayou_1_1_0 synthesizer = new SynthesizerBayou_1_1_0(bayouHostname, 8080);
 
-        new FloodGage().run(synthesizer, new ViewConsole());
-
-        System.out.println("");
+        try
+        {
+            new FloodGage().run(synthesizer, new ViewConsole());
+        }
+        finally
+        {
+            System.out.println(""); // so that visually the command prompt on exit is not cluttered by our output
+        }
     }
-
 }
