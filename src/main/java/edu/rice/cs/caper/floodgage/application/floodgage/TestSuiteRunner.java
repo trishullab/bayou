c37a77d0e2e4911061fd70ccef596e51166af52f
R. Matt Barnett <barnett@rice.edu>
2017-11-21 18:25:58 -0600

    doc and cleanup of edu.rice.cs.caper.floodgage.application.floodgage

--- src/main/java/edu/rice/cs/caper/floodgage/application/floodgage/TestSuiteRunner.java
+++ src/main/java/edu/rice/cs/caper/floodgage/application/floodgage/TestSuiteRunner.java
@@ -1,22 +1,31 @@
 package edu.rice.cs.caper.floodgage.application.floodgage;
 
-import edu.rice.cs.caper.floodgage.application.floodgage.model.plan.TestSuite;
 import edu.rice.cs.caper.floodgage.application.floodgage.view.View;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.security.spec.ECField;
-
+/**
+ * Wrapper for runTestSuiteAgainst(...)
+ */
 class TestSuiteRunner
 {
+    /**
+     * Creates and runs a JUnit test runner for testSuite.
+     *
+     * @param testSuite the class defining test cases to run
+     * @param view a UI component to report test failures to
+     * @return if all test cases passed
+     */
     static boolean runTestSuiteAgainst(Class testSuite, View view)
     {
-        Result result = new JUnitCore().run(testSuite);
+        if(testSuite == null)
+            throw new NullPointerException("testSuite");
 
+        if(view == null)
+            throw new NullPointerException("view");
+
+        Result result = new JUnitCore().run(testSuite);
 
         if (result.getFailureCount() > 0)
         {
