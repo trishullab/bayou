10de459e656de21a0f5c37e33fa2628b1d32e00f
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-01-09 11:36:13 -0500

    Build floodgage if it does not exist already.

--- tool_files/floodgage/floodgage.sh
+++ tool_files/floodgage/floodgage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+test -f floodgage-1.0.0.jar || (cd ../maven_3_3_9/floodgage && mvn package&& cp target/floodgage-1.0.0-jar-with-dependencies.jar ../../floodgage/floodgage-1.0.0.jar)
 java -cp floodgage-1.0.0.jar:$1 edu.rice.cs.caper.floodgage.application.floodgage.FloodGageMain $2
 
 
