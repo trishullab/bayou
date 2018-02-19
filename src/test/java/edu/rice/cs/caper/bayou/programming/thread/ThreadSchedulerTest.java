874ba9c3771195ce7a3fc948ad21ea79b57e3261
R. Matt Barnett <barnett@rice.edu>
2018-02-19 14:08:19 -0600

    add unit tests

--- src/test/java/edu/rice/cs/caper/bayou/programming/thread/ThreadSchedulerTest.java
+++ src/test/java/edu/rice/cs/caper/bayou/programming/thread/ThreadSchedulerTest.java
@@ -6,6 +6,7 @@ import org.eclipse.jetty.util.BlockingArrayQueue;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.BlockingQueue;
@@ -18,27 +19,64 @@ public abstract class ThreadSchedulerTest
     {
         ThreadScheduler scheduler = makeScheduler();
 
-        List<Integer> queue = new Vector<>();
+        List<Integer> numbers = new Vector<>();
 
         scheduler.schedule(() -> {
-            queue.add(1);
+            numbers.add(1);
             return null;
         });
 
         scheduler.schedule(() -> {
-            queue.add(2);
+            numbers.add(2);
             return null;
         });
 
         scheduler.schedule(() -> {
-            queue.add(3);
+            numbers.add(3);
             return null;
         });
 
-        Assert.assertEquals(new Integer(1), queue.get(0));
-        Assert.assertEquals(new Integer(2), queue.get(1));
-        Assert.assertEquals(new Integer(3), queue.get(2));
+        Assert.assertEquals(new Integer(1), numbers.get(0));
+        Assert.assertEquals(new Integer(2), numbers.get(1));
+        Assert.assertEquals(new Integer(3), numbers.get(2));
     }
+
+    @Test
+    public void scheduleTest() throws InterruptedException
+    {
+        ThreadScheduler scheduler = makeScheduler();
+
+        List<Integer> numbers = new Vector<>();
+
+        Thread t1 = launchThread(() -> scheduler.schedule(() -> {
+            numbers.add(1);
+            return null;
+        }));
+
+        Thread t2 = launchThread(() -> scheduler.schedule(() -> {
+            numbers.add(2);
+            return null;
+        }));
+
+        Thread t3= launchThread(() -> scheduler.schedule(() -> {
+            numbers.add(3);
+            return null;
+        }));
+
+        t1.join();
+        t2.join();
+        t3.join();
+
+        Assert.assertTrue(numbers.containsAll(Arrays.asList(1, 2, 3)));
+    }
+
+    private Thread launchThread(Runnable runnable)
+    {
+        Thread t =  new Thread(runnable);
+        t.start();
+        return t;
+    }
+
 
     protected abstract ThreadScheduler makeScheduler();
 }
