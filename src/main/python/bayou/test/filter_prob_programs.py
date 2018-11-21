84ea91092dd9ce3149de90c8f6b03950d6dc022b
Letao Qi <letao.qi@gmail.com>
2018-11-20 22:08:43 -0600

    Add filter function to filter_prob_programs.py

--- src/main/python/bayou/test/filter_prob_programs.py
+++ src/main/python/bayou/test/filter_prob_programs.py
@@ -11,9 +11,28 @@ def compute_stats(clargs):
         ast = prog['ast']
         if 'cond_prob' in ast:
             probs.append(ast['cond_prob'])
-    print('total number of useful programs is %' % len(probs))
+    print('total number of useful programs is %i' % len(probs))
     probs = sorted(probs, reverse=True)
     import pdb; pdb.set_trace()
+
+
+def filter_progs(clargs, reserve_num=400000):
+    input_file = clargs.input_file
+    with open(input_file) as f:
+        progs = json.load(f)['programs']
+    progs_probs = {}
+    for prog in progs:
+        ast = prog['ast']
+        if 'cond_prob' in ast:
+            progs_probs[prog] = ast['cond_prob']
+    print('total number of useful programs is %i' % len(progs_probs))
+    progs_probs_items = sorted(progs_probs, key=lambda kv: kv[1], reverse=True)
+    reserved_progs = [prog for (prog, prob) in progs_probs_items[:reserve_num]]
+    print('number of reserved programs is %i' % len(reserved_progs))
+    import pdb; pdb.set_trace()
+    with open(clargs.out, 'w') as f:
+        json.dump({'programs': reserved_progs}, f, indent=2)
+    print('done')
 
 
 if __name__ == '__main__':
@@ -21,4 +40,5 @@ if __name__ == '__main__':
     parser.add_argument('input_file', type=str, help='input data file')
     parser.add_argument('--out', type=str, default='temp.out')
     clargs = parser.parse_args()
-    compute_stats(clargs)
+    # compute_stats(clargs)
+    filter_progs(clargs)
